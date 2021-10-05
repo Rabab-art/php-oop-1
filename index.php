841cc71b29a5537641743c79be44048c335331d2
Rabab-art <bouhamla.rabab@gmail.com>
2021-10-05 15:16:33 +0200

    Dichirazione delle variabili d'istanza

---
+++ index.php
@@ -0,0 +1,47 @@
+<!-- - è definita una classe ‘Movie’
+   => all'interno della classe sono dichiarate delle variabili d'istanza (proprietà)
+   => all'interno della classe è definito un costruttore
+   => all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori 
+delle relative proprietà, se possibile cerchiamo di stampare senza ricorrere a var_dump -->
+
+
+<?php 
+
+class Movie
+{
+  
+  public $title; 
+  public $director; 
+  public $year; 
+
+public function __construct()
+{
+    
+}
+
+public function presentazione()
+{
+    echo "Ciao i miei film preferiti sono $this->title";
+}
+
+
+
+}
+
+// Istanza n.1
+
+$mammaMia = new Movie();
+$mammaMia->title = "Mamma mia!";
+$mammaMia->director ="Phyllida Lloyd";
+$mammaMia->year = "1999";
+
+// Istanza n.2
+
+$wanted = new Movie();
+$wanted->title="Wanted";
+$wanted->director= "Timur Bekmambetov";
+$wanted->year = "2008";
+
+
+
